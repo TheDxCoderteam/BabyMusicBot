961d6716028f3e0c5bf5cabed05cf214b57cbdad
DX CODER <166283380+TheDxCoderteam@users.noreply.github.com>
2024-07-03 07:30:39 +0530

    Love 💕

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import re
+ import re
 from os import getenv
 
 from dotenv import load_dotenv
@@ -7,12 +7,12 @@ from pyrogram import filters
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI",None)
+API_ID = int(getenv("API_ID",28151931))
+API_HASH = getenv("API_HASH","8dd03f69a5e342ebd65f077fb4aa97a8")
+BOT_TOKEN = getenv("BOT_TOKEN","6720481419:AAEGGuj2zLLZ7rbJ1uzpba0iKZY6v_J-2Yg")
+MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://Aryan:Aryanmusic@aryanmusic0.qzy20ty.mongodb.net/?retryWrites=true&w=majority")
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1080))
-LOGGER_ID = int(getenv("LOGGER_ID", None ))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002140883754 ))
 OWNER_USERNAME = getenv("OWNER_USERNAME","Itz_prince_king")
 CHAT_GROUP = getenv("CHAT_GROUP","https://t.me/FRIENDS2FAMILY00")
 OWNER_ID = int(getenv("OWNER_ID", 5724322712))
@@ -33,7 +33,7 @@ PROMO = getenv("PROMO","https://graph.org/file/0e63ef1e60dd367a3daa5.jpg")
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 
-STRING1 = getenv("STRING_SESSION",  None)
+STRING1 = getenv("STRING_SESSION",  "BQGtkHsAPqmZMtaRuoTLOuBFQ7DDGbV9DiEJGTgkoRw6WzOPmncydJCm-VXxRTAp2McRxk__1R_QFwvqNakkbmyHbBrZEHY9_DZf3j9MrpHN_Z-yq44s6JeIXWzUb2DYpbbLxcdA3JQ6SsemBcfN988t4FUq2q3_OFqJl5cRIkyT5mhjFbG8XqNPwjC8TubnMj2ms_QUxc5-Oub_cJwElho8_KUFYpCacs53YpWyxV61P5hhVNt8rxKXS0-QYY98V-NI_z9fqTvLhQuHRnVuj_Qya498JT-nQt_qP6MYpCxeCFSgl6yLsUGShVjomPN7fbrOGdyHeUu6NOXeSXA4FWtmEbW7vQAAAAGk1NVAAA")
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
